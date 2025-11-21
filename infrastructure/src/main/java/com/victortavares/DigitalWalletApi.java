dcf54c68f5486430a26fe2bef6260d3c8e37df9f
Victor <victavares2019@gmail.com>
2025-11-21 18:33:04 -0300

    config: add framework dependencies

---
+++ infrastructure/src/main/java/com/victortavares/DigitalWalletApi.java
@@ -0,0 +1,12 @@
+package com.victortavares;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalWalletApi {
+
+    public static void main(String[] args){
+        SpringApplication.run(DigitalWalletApi.class, args);
+    }
+}
