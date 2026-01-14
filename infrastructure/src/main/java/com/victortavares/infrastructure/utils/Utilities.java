5765f9390a0c699363cfe1bf3a74e18de2d2d1a6
Victor <victavares2019@gmail.com>
2026-01-14 00:32:55 -0300

    chore(utils): create a utility class

---
+++ infrastructure/src/main/java/com/victortavares/infrastructure/utils/Utilities.java
@@ -0,0 +1,9 @@
+package com.victortavares.infrastructure.utils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Utilities {
+
+    public static Logger log = LoggerFactory.getLogger("digital-wallet-api");
+}
