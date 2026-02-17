0879c58ff68bae4139f5b2c1f4ea30a0cac21f22
Victor <victavares2019@gmail.com>
2026-02-16 21:42:45 -0300

    chore(service): implement tax number available gateway

---
+++ infrastructure/src/main/java/com/victortavares/infrastructure/service/TaxNumberAvailableGatewayImpl.java
@@ -0,0 +1,23 @@
+package com.victortavares.infrastructure.service;
+
+import com.victortavares.geteway.TaxNumberAvailableGateway;
+import com.victortavares.infrastructure.repository.UserEntityRepository;
+import org.springframework.stereotype.Service;
+
+import static com.victortavares.infrastructure.utils.Utilities.log;
+
+@Service
+public class TaxNumberAvailableGatewayImpl implements TaxNumberAvailableGateway {
+
+    private UserEntityRepository userEntityRepository;
+
+    public TaxNumberAvailableGatewayImpl(UserEntityRepository userEntityRepository) {
+        this.userEntityRepository = userEntityRepository;
+    }
+
+    @Override
+    public Boolean TaxNumberAvailable(String taxNumber) {
+        log.info("Verifying if tax number is disponible");
+        return !userEntityRepository.existsByTaxNumber(taxNumber);
+    }
+}
