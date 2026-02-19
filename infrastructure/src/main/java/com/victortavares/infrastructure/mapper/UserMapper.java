f7d1fb069fe96d627d3be55a0fe63c14b6906564
Victor <victavares2019@gmail.com>
2026-02-18 21:22:31 -0300

    feat(TransactionPinValidate): It uses polymorphism in `toUser` to transform the entity into the user model

--- infrastructure/src/main/java/com/victortavares/infrastructure/mapper/UserMapper.java
+++ infrastructure/src/main/java/com/victortavares/infrastructure/mapper/UserMapper.java
@@ -31,4 +31,17 @@ public class UserMapper {
             request.type()
         );
     }
+
+    public User toUser(UserEntity userEntity) throws Exception {
+        return new User(
+                userEntity.getId(),
+                userEntity.getEmail(),
+                userEntity.getPassword(),
+                new TaxNumber(userEntity.getTaxNumber()),
+                userEntity.getFullName(),
+                userEntity.getType(),
+                userEntity.getCreatedAt(),
+                userEntity.getUpdateAt()
+        );
+    }
 }
