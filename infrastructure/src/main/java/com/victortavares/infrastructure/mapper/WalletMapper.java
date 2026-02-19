3c27293eccc4c924c84f6d980bd2199e77c0e186
Victor <victavares2019@gmail.com>
2026-02-18 21:14:33 -0300

    feat(mapper): create a method to transform an entity into the Wallet model.

--- infrastructure/src/main/java/com/victortavares/infrastructure/mapper/WalletMapper.java
+++ infrastructure/src/main/java/com/victortavares/infrastructure/mapper/WalletMapper.java
@@ -9,6 +9,14 @@ import org.springframework.stereotype.Component;
 @Component
 public class WalletMapper {
 
+    private TransactionPinMapper transactionPinMapper;
+    private UserMapper userMapper;
+
+    public WalletMapper(TransactionPinMapper transactionPinMapper, UserMapper userMapper) {
+        this.transactionPinMapper = transactionPinMapper;
+        this.userMapper = userMapper;
+    }
+
     public WalletEntity toWalletEntity(Wallet wallet, UserEntity userEntity, TransactionPinEntity transactionPinEntity){
         return new WalletEntity(
                 wallet.getBalance(),
@@ -16,6 +24,21 @@ public class WalletMapper {
                 transactionPinEntity,
                 wallet.getCreatedAt(),
                 wallet.getUpdateAt()
+        );
+    }
+
+    public Wallet toWallet(WalletEntity walletEntity) throws Exception {
+        if (walletEntity == null){
+            return null;
+        }
+
+        return new Wallet(
+            walletEntity.getId(),
+            walletEntity.getBalance(),
+            userMapper.toUser(walletEntity.getUserEntity()),
+            walletEntity.getCreateAt(),
+            walletEntity.getUpdateAt(),
+            transactionPinMapper.toTransactionPin(walletEntity.getTransactionPinEntity())
         );
     }
 
