33c45cc2fd45138dd2137a08442c6a743e887ab3
Victor <victavares2019@gmail.com>
2026-02-18 21:20:36 -0300

    feat(TransactionPinValidate): Implement Gateway to this action

--- infrastructure/src/main/java/com/victortavares/infrastructure/mapper/TransactionPinMapper.java
+++ infrastructure/src/main/java/com/victortavares/infrastructure/mapper/TransactionPinMapper.java
@@ -16,4 +16,16 @@ public class TransactionPinMapper {
                 transactionPin.getUpdateAt()
         );
     }
+
+    public TransactionPin toTransactionPin(TransactionPinEntity transactionPinEntity) {
+        return new TransactionPin(
+            transactionPinEntity.getId(),
+            transactionPinEntity.getPin(),
+            transactionPinEntity.getAttempt(),
+            transactionPinEntity.getBlocked(),
+            transactionPinEntity.getCreateAt(),
+            transactionPinEntity.getUpdateAt()
+        );
+
+    }
 }
---
+++ infrastructure/src/main/java/com/victortavares/infrastructure/service/TransactionValidateGatewayImpl.java
@@ -0,0 +1,25 @@
+package com.victortavares.infrastructure.service;
+
+import com.victortavares.core.domain.Transaction;
+import com.victortavares.geteway.TransactionValidateGateway;
+import com.victortavares.infrastructure.client.ApiValidateService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TransactionValidateGatewayImpl implements TransactionValidateGateway {
+
+    private ApiValidateService apiValidateService;
+
+    public TransactionValidateGatewayImpl(ApiValidateService apiValidateService) {
+        this.apiValidateService = apiValidateService;
+    }
+
+    @Override
+    public Boolean validate(Transaction transaction) {
+        var response = apiValidateService.validate();
+        if (response == null){
+            return false;
+        }
+        return response.success();
+    }
+}
