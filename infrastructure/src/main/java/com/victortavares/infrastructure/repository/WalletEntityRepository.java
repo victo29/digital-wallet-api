10afa0729333d638b403a6c67994042c9eaafee9
Victor <victavares2019@gmail.com>
2026-02-18 21:23:56 -0300

    feat(repository): create method to find wallet by tax number

--- infrastructure/src/main/java/com/victortavares/infrastructure/repository/WalletEntityRepository.java
+++ infrastructure/src/main/java/com/victortavares/infrastructure/repository/WalletEntityRepository.java
@@ -4,4 +4,6 @@ import com.victortavares.infrastructure.entity.WalletEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface WalletEntityRepository extends JpaRepository<WalletEntity, Long> {
+
+    WalletEntity findByUserEntityTaxNumber(String taxNumber);
 }
